849a634f2c2d04357be1c0709acce6df8dfd8aea
Bartek Pacia <barpac02@gmail.com>
2020-10-09 14:29:20 +0200

    updated about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,23 +4,24 @@ import Layout from "../components/layout"
 const AboutPage = () => (
   <Layout>
     <h1>O nas</h1>
-    <p>Zarząd:</p>
-    <ul>
-      <li>Chwastek Rafał</li>
-      <li>Dudek Małgorzata</li>
-      <li>Jarosław Salomon</li>
-      <li>Kisiel Adrian </li>
-      <li>Kucjas Krzysztof</li>
-      <li>Piecha Katarzyna </li>
-      <li>Pojda Aleksander</li>
-      <li> Szulik Grażyna</li>
-    </ul>
-    <p>Komisja rewizyjna:</p>
-    <ul>
-      <li>Szulik Jacek</li>
-      <li>Delong-Michałowska Izabela</li>
-      <li>Wita Michał</li>
-    </ul>
+    <p>Skład Zarządu MSZS w Rybniku (2020-2024)</p>
+    <ol>
+      <li>Rafał Chwastek</li>
+      <li>Małgorzata Dudek</li>
+      <li>Ksawery Fojcik</li>
+      <li>Michał Kowalczyk-Kudziarz</li>
+      <li>Krzysztof Kucjas</li>
+      <li>Katarzyna Piecha</li>
+      <li>Jarosław Salamon</li>
+      <li>Jacek Szulik</li>
+      <li>Grażyna Szulik – Prezes Miejskiego Szkolnego Związku Sportowego w Rybniku</li>
+    </ol>
+    <p>Skład Komisji Rewizyjnej MSZS w Rybniku (2020-2024)</p>
+    <ol>
+      <li>Michał Wita - Przewodniczący</li>
+      <li>Marcin Krawczyński</li>
+      <li>Izabela Delong - Michałowska</li>
+    </ol>
   </Layout>
 )
 
